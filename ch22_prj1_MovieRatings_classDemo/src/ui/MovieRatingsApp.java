2a01928061271d6551577533ebc206a737a0f3eb
Sean Blessing <snblessing@gmail.com>
2019-08-28 17:07:33 -0400

    catchup commit

--- ch22_prj1_MovieRatings_classDemo/src/ui/MovieRatingsApp.java
+++ ch22_prj1_MovieRatings_classDemo/src/ui/MovieRatingsApp.java
@@ -9,6 +9,7 @@ import util.Console;
 
 public class MovieRatingsApp {
 
+	
 	public static void main(String[] args) {
 		//initialize stuff
 		MovieCollection mc = new MovieCollection();
@@ -21,76 +22,6 @@ public class MovieRatingsApp {
 		mc.add(new Movie("Back to the Future Part III", 1990, 2.1));
 		mc.add(new Movie("Entourage", 2015, 1.5));
 		
-		System.out.println("Welcome to the Movie Ratings application!\n");
-		int choice = 0;
-		
-		while (choice!=6) {
-			displayMenu();
-		
-			choice = Console.getInt("Choose a menu option:  ", 1, 6);
-		
-			List<Movie> filteredMovies;
-			switch (choice) {
-				case 1:		// Enter a movie
-					int nbr = Console.getInt("How many movies do you want to enter?  ", 0, 11);
-					for (int i = 1; i <= nbr; i++) {
-						Console.println("Movie # "+i);
-						Console.println("===========");
-						String title = Console.getString("Enter title:  ");
-						int year = Console.getInt("Enter year:  ", 1900, LocalDate.now().getYear());
-						double rating = Console.getDouble("Enter rating:  ", 1.0, 5.0);
-						Movie m = new Movie(title, year, rating);
-						mc.add(m);
-					}
-					break;
-				case 2:		// View top rated movies
-					filteredMovies = mc.filterMovies(m -> m.getRating()>=4.0);
-					Console.println("Movies rated 4.0 or higher");
-					Console.println("==========================");
-					filteredMovies.stream().forEach(System.out::println);
-					break;
-				case 3:		// View most recent movies (movies released within last 10 years)
-					int yearRange = LocalDate.now().getYear() - 10;
-					filteredMovies = mc.filterMovies(m -> m.getYear()>=yearRange);
-					Console.println("Movies released in last 10 years");
-					Console.println("================================");
-					filteredMovies.stream().forEach(System.out::println);
-					break;
-				case 4:
-					Console.println("All movies");
-					Console.println("==========");
-					mc.getMovies().stream().forEach(System.out::println);
-					break;
-				case 5:		// View ratings
-					Console.println("Movie rating data");
-					Console.println("=================");
-					Console.println("# of movies:  "+mc.getSize());
-					Console.println("lowest rating:  "+mc.getLowestRating());
-					Console.println("highest rating:  "+mc.getHighestRating());
-					Console.println("average rating:  "+mc.getAverageRating());
-					break;
-				case 6:
-					Console.println("Goodbye!");
-					break;
-			}
-		
-		}
-		
-
-	}
-
-	public static void displayMenu() {
-        Console.println("-----------------------");
-        Console.println("What do you want to do?");
-        Console.println("-----------------------");
-        Console.println("1 - Enter a movie");
-        Console.println("2 - View top rated movies");
-        Console.println("3 - View most recent movies");
-        Console.println("4 - View all movies");
-        Console.println("5 - View ratings");
-        Console.println("6 - Quit application");
-        
-        Console.println();
 	}
 	
 }
